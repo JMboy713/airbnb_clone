f5d1cd7244f8a69d3b35701a6d1a6045514d24cf
Jmboy713 <jimin713@naver.com>
2023-10-13 13:32:59 +0900

    startProject,make admin

---
+++ houses/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import House
+
+@admin.register(House) # admin이 관리하게 될 모델은 House라고 등록. 
+class HouseAdmin(admin.ModelAdmin): # model 관리자를 상속
+	list_display = ["name","price_per_night","description","address","pets_allowed"] # 위에 이름 붙이기.
+	list_filter=["price_per_night","pets_allowed"]# 필터 만들어주기. 
+	search_fields=["address"] # startswith 를 뒤에 붙이면 시작하는 문자로 찾을 수 있다. 
