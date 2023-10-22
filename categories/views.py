c9fc4eae5a135478f344c1b08e5a36172ed05f53
Jmboy713 <jimin713@naver.com>
2023-10-22 15:15:38 +0900

    django rest framework

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Category
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view()
+def categories(request):
+    all_categories=Category.objects.all()
+    return Response({'ok':True,})
+	# return JsonResponse({'ok':True,'cate':all_catetories}) -> 쿼리셋은 json으로 바로 못준다.. json 포맷으로 바꿔주어어야 한다.
--- config/urls.py
+++ config/urls.py
@@ -6,4 +6,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('rooms/', include("rooms.urls")), # 처음에 갈 곳, 실행할 함수.
+    path('categories/',include("categories.urls")),
 ]
